506f5f13f653bfe722566f4b834a335928d91846
logonzalez <lalzate@enerbit.co>
2024-12-02 14:22:21 -0500

    init commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+mod models;
+mod handlers;
+
+use actix_web::{web, App, HttpServer};
+
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(
+                web::scope("/api")
+                .route("/items", web::post().to(handlers::create_item))
+                .route("/items", web::get().to(handlers::get_items))
+                .route("/items/{id}", web::get().to(handlers::get_item))
+                .route("/items/{id}", web::put().to(handlers::update_item))
+                .route("/items/{id}", web::delete().to(handlers::delete_item))
+            )
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+}
